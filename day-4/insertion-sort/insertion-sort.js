448851a224e530a1522f47e67a07e6938f5350c5
Jayvic San Antonio <codes@jayvicsanantonio.dev>
2023-08-31 22:10:14 -0700

    Insertion Sort

---
+++ day-4/insertion-sort/insertion-sort.js
@@ -0,0 +1,16 @@
+function insertionSort(arr) {
+  for (let i = 1; i < arr.length; i++) {
+    const currentValue = arr[i];
+    let j = i - 1;
+
+    while (j >= 0 && arr[j] > currentValue) {
+      arr[j + 1] = arr[j];
+      j--;
+    }
+
+    arr[j + 1] = currentValue;
+  }
+  return arr;
+}
+
+export default insertionSort;
