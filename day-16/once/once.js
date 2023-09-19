ca5be79ad7e7c46cfc12a83d7bc75ec2d4eb4dd4
Jayvic San Antonio <codes@jayvicsanantonio.dev>
2023-09-19 14:01:04 -0700

    Once

---
+++ day-16/once/once.js
@@ -0,0 +1,13 @@
+export default function once(fn) {
+  let ranOnce = false;
+  let result = null;
+
+  return function (...args) {
+    if (!ranOnce) {
+      result = fn.apply(this, args);
+      ranOnce = true;
+    }
+
+    return result;
+  };
+}
