f13c855e4f8468d6be218c4ef8bfee7748f906c1
Jayvic San Antonio <codes@jayvicsanantonio.dev>
2023-09-27 00:03:18 -0700

    Debounce

---
+++ day-22/debounce/debounce.js
@@ -0,0 +1,13 @@
+export default function debounce(fn, delay) {
+  let timeoutId = null;
+
+  return function (...args) {
+    if (timeoutId) {
+      clearTimeout(timeoutId);
+    }
+
+    timeoutId = setTimeout(() => {
+      fn.apply(this, args);
+    }, delay);
+  };
+}
