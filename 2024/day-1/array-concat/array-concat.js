e975f352f2c5899f7747c4e6df9e0748410fb2bb
Jayvic San Antonio <codes@jayvicsanantonio.dev>
2024-01-01 23:29:51 -0800

    Array Concat

---
+++ 2024/day-1/array-concat/array-concat.js
@@ -0,0 +1,13 @@
+Array.prototype.myConcat = function (...items) {
+  const newArray = [...this];
+
+  for (let i = 0; i < items.length; i++) {
+    if (Array.isArray(items[i])) {
+      newArray.push(...items[i]);
+    } else {
+      newArray.push(items[i]);
+    }
+  }
+
+  return newArray;
+};
