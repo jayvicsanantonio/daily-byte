082db711e368e243b9469e011643b1f2adc4d5e7
Jayvic San Antonio <codes@jayvicsanantonio.dev>
2024-01-02 23:37:11 -0800

    Cancellable Timeout

---
+++ 2024/day-2/cancellable-timeout/cancellable-timeout.js
@@ -0,0 +1,9 @@
+export default function setCancellableTimeout(callback, delay, ...args) {
+  const timeoutId = setTimeout(() => {
+    callback(...args);
+  }, delay);
+
+  return () => {
+    clearTimeout(timeoutId);
+  };
+}
