516fff91f68053416ca7a2cf14ae220b8f2e8798
Jayvic San Antonio <codes@jayvicsanantonio.dev>
2024-01-03 21:29:27 -0800

    Drop While

---
+++ 2024/day-3/drop-while/drop-while.js
@@ -0,0 +1,9 @@
+export default function dropWhile(array, predicate) {
+  let index = 0;
+
+  while (index < array.length && predicate(array[index], index, array)) {
+    index++;
+  }
+
+  return array.slice(index);
+}
