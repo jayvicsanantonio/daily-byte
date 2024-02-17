abe61a7524c9f2631e4cf970890f42846bb96af5
Jayvic San Antonio <codes@jayvicsanantonio.dev>
2024-02-16 23:37:39 -0800

    Find Last Index

---
+++ 2024/day-39/find-last-index/find-last-index.js
@@ -0,0 +1,19 @@
+export default function findLastIndex(
+  array,
+  predicate,
+  fromIndex = array.length - 1
+) {
+  const length = array.length;
+  const startIndex =
+    fromIndex < 0
+      ? Math.max(array.length + fromIndex, 0)
+      : Math.min(fromIndex, array.length - 1);
+
+  for (let i = startIndex; i >= 0; i--) {
+    if (predicate(array[i], i, array)) {
+      return i;
+    }
+  }
+
+  return -1;
+}
