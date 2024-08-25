21be25bf0c5a0eef7a36c2cf4f37724f60bce0c5
Jayvic San Antonio <codes@jayvicsanantonio.dev>
2024-08-24 21:47:23 -0700

    Contact Form

---
+++ 2024/half-2/day-1/contact-form/src/App.tsx
@@ -0,0 +1,23 @@
+import submitForm from "./submitForm";
+
+export default function App() {
+  return (
+    <form onSubmit={submitForm} action="http://localhost:5173/" method="post">
+      <div>
+        <label htmlFor="name-input">Name</label>
+        <input id="name-input" name="name" type="text" />
+      </div>
+      <div>
+        <label htmlFor="email-input">Email</label>
+        <input id="email-input" name="email" type="email" />
+      </div>
+      <div>
+        <label htmlFor="message-input">Message</label>
+        <textarea id="message-input" name="message"></textarea>
+      </div>
+      <div>
+        <button>Send</button>
+      </div>
+    </form>
+  );
+}
