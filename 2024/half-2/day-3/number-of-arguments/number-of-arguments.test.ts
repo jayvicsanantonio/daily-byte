7323f30e87c439ea42adf6a6a64a5a424d97a947
Jayvic San Antonio <codes@jayvicsanantonio.dev>
2024-08-29 09:36:13 -0700

    Number of Arguments

---
+++ 2024/half-2/day-3/number-of-arguments/number-of-arguments.test.ts
@@ -0,0 +1,16 @@
+import { describe, expect, test } from "vitest";
+import numberOfArguments from "./number-of-arguments";
+
+describe("numberOfArguments", () => {
+  test("no arguments", () => {
+    expect(numberOfArguments()).toBe(0);
+  });
+
+  test("one argument", () => {
+    expect(numberOfArguments(0)).toBe(1);
+  });
+
+  test("two arguments", () => {
+    expect(numberOfArguments(0, 1)).toBe(2);
+  });
+});
