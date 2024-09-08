e34f2ff887d13eee0b579fc4c36afa1f6d402d5a
Jayvic San Antonio <codes@jayvicsanantonio.dev>
2024-09-07 23:51:12 -0700

    Drop While

---
+++ 2024/half-2/day-5/drop-while/drop-while.test.ts
@@ -0,0 +1,16 @@
+import { describe, test, expect } from "vitest";
+import dropWhile from "./drop-while";
+
+describe("dropWhile", () => {
+  test("empty array", () => {
+    expect(dropWhile([], (value) => value < 3)).toEqual([]);
+  });
+
+  test("drop some elements", () => {
+    expect(dropWhile([1, 2, 3, 4, 5], (value) => value < 3)).toEqual([3, 4, 5]);
+  });
+
+  test("drop everything", () => {
+    expect(dropWhile([1, 2, 3, 4, 5], () => true)).toEqual([]);
+  });
+});
