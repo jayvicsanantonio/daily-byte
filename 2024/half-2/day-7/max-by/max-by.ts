42eee4c63bfbbebcd8becf63bf23edc3845276cf
Jayvic San Antonio <codes@jayvicsanantonio.dev>
2024-09-14 21:58:21 -0700

    Address Max By

--- 2024/half-2/day-7/max-by/max-by.ts
+++ 2024/half-2/day-7/max-by/max-by.ts
@@ -1,12 +1,22 @@
-export default function maxBy(array: Array<any>, iteratee: Function): any {
-  let result: any, computed: any;
+export default function maxBy<T, U>(
+  array: Array<T>,
+  iteratee: (value: T) => U
+): T | undefined {
+  let result: T | undefined;
+  let computed: U | undefined;
 
   for (const value of array) {
-    const current: any = iteratee(value);
-    if (current != null && (computed === undefined || current > computed)) {
+    const current = iteratee(value);
+
+    if (
+      current !== null &&
+      current !== undefined &&
+      (computed === undefined || (computed !== null && current > computed))
+    ) {
       computed = current;
       result = value;
     }
   }
+
   return result;
 }
