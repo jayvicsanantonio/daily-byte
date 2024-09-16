21cd35694a5bbdedee9c96db62508f374a4dbebc
Jayvic San Antonio <codes@jayvicsanantonio.dev>
2024-09-15 23:37:35 -0700

    Min By

---
+++ 2024/half-2/day-7/min-by/min-by.test.ts
@@ -0,0 +1,22 @@
+import { describe, test, expect } from "vitest";
+import minBy from "./min-by";
+
+describe("minBy", () => {
+  test("empty input array", () => {
+    expect(minBy([], () => {})).toEqual(undefined);
+  });
+
+  test("one element", () => {
+    expect(minBy([{ n: 1 }], (o) => o.n)).toEqual({ n: 1 });
+  });
+
+  test("two elements", () => {
+    expect(minBy([{ n: 1 }, { n: 2 }], (o) => o.n)).toEqual({ n: 1 });
+    expect(
+      minBy(
+        [{ data: { score: 20 } }, { data: { score: 10 } }],
+        (o) => o.data.score
+      )
+    ).toEqual({ data: { score: 10 } });
+  });
+});
