50c2c6b21a083770b059b9bc01d7ca4a954267b9
Jayvic San Antonio <codes@jayvicsanantonio.dev>
2024-11-05 08:50:09 -0800

    Address review comments

--- 2024/11/4/array-square/array-square.ts
+++ 2024/11/4/array-square/array-square.ts
@@ -1,7 +1,17 @@
-interface Array<T> {
+interface Array<T extends number> {
   square(): Array<T>;
 }
 
 Array.prototype.square = function () {
-  return this.map((value) => value ** 2);
+  return this.map((value) => {
+    if (!Number.isFinite(value)) {
+      throw new Error("Array square can only be called on finite numbers");
+    }
+
+    if (Math.abs(value) > Math.sqrt(Number.MAX_SAFE_INTEGER)) {
+      throw new Error("Square operation exceeds maximum safe integer");
+    }
+
+    return value ** 2;
+  });
 };
--- 2024/11/4/cancellable-timeout/cancellable-timeout.ts
+++ 2024/11/4/cancellable-timeout/cancellable-timeout.ts
@@ -1,11 +1,19 @@
-export default function setCancellableTimeout(
-  callback: Function,
+export default function setCancellableTimeout<TArgs extends Array<any>>(
+  callback: (...args: TArgs) => void,
   delay?: number,
-  ...args: Array<any>
+  ...args: TArgs
 ) {
-  let timeoutId = setTimeout(callback, delay, ...args);
+  let timeoutId: ReturnType<typeof setTimeout> | undefined = setTimeout(
+    callback,
+    delay,
+    ...args
+  );
+  let cancelled = false;
 
   return function cancel() {
+    if (cancelled) return;
     clearTimeout(timeoutId);
+    cancelled = true;
+    timeoutId = undefined;
   };
 }
