b612375a944809a622c19d607b1d830298a214bc
Jayvic San Antonio <codes@jayvicsanantonio.dev>
2024-11-05 10:01:46 -0800

    Chunk

---
+++ 2024/11/5/chunk.ts
@@ -0,0 +1,18 @@
+export default function chunk<T>(
+  array: Array<T>,
+  size: number = 1
+): Array<Array<T>> {
+  if (!Array.isArray(array) || size < 1) {
+    return [];
+  }
+
+  const result = [];
+
+  for (let i = 0; i < array.length; i += size) {
+    const chunk = array.slice(i, i + size);
+
+    result.push(chunk);
+  }
+
+  return result;
+}
